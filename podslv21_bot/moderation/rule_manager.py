4283c944e86cdc76c0dbcc8378ca9c9d0a59539b
Librellium <246878136+librellium@users.noreply.github.com>
2025-12-02 00:52:33 +0500

    Update RuleManager rule loading logic

--- podslv21_bot/moderation/rule_manager.py
+++ podslv21_bot/moderation/rule_manager.py
@@ -1,3 +1,4 @@
+import logging
 from os import listdir
 from pathlib import Path
 from typing import List
@@ -5,18 +6,24 @@ from typing import List
 
 class RuleManager:
     def __init__(self, rules_dir: Path):
+        self._logger = logging.getLogger("podslv21_bot.moderation.rule_manager")
+
         self.rules_dir = rules_dir
-        self.rules: List[str] = []
+        self._rules: List[str] = []
 
     def reload(self):
         if not self.rules_dir.is_dir():
             return
 
-        self.rules.clear()
+        self._rules.clear()
         for rule_filename in listdir(self.rules_dir):
             rule_file = Path(self.rules_dir / rule_filename).resolve()
             with rule_file.open(encoding="utf-8") as f:
-                self.rules.append(f.read())
+                rule = f.read()
+                if rule:
+                    self._rules.append(rule)
+
+        self._logger.info(f"Rules loaded total={len(self._rules)}")
 
     def get_rules(self):
-        return self.rules
+        return self._rules
