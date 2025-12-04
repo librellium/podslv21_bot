b90f0c3ea1832f9a223b3ae5418157b80913bd28
Librellium <246878136+librellium@users.noreply.github.com>
2025-12-04 21:01:17 +0500

    Refactor TemplateRenderer for readability

--- anonflow/bot/utils/template_renderer.py
+++ anonflow/bot/utils/template_renderer.py
@@ -1,32 +1,46 @@
 import logging
 from pathlib import Path
+from typing import Optional
 
 from aiogram.types import Message
 from jinja2 import Environment, FileSystemLoader, TemplateNotFound
 
+from anonflow import __version_str__
+
 
 class TemplateRenderer:
-    def __init__(self, templates_path: Path):
+    def __init__(self, templates_dir: Path):
         self._logger = logging.getLogger(__name__)
 
         self._env = Environment(
-            loader=FileSystemLoader(templates_path),
+            loader=FileSystemLoader(templates_dir),
             enable_async=True
         )
 
     async def _get_context(self, message: Message):
+        bot = await message.bot.get_me()
+        user = message.from_user
+
         return {
-            "first_name": message.from_user.first_name,
-            "last_name": message.from_user.last_name,
-            "username": message.from_user.username
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "username": user.username,
+            "bot_first_name": bot.first_name,
+            "bot_last_name": bot.last_name,
+            "bot_username": bot.username,
+            "bot_version": __version_str__
         }
 
-    async def render(self, template_name: str, message: Message):
+    async def render(self,
+                     template_name: str,
+                     *,
+                     message: Optional[Message] = None,
+                     **extra_context):
         try:
             template = self._env.get_template(template_name)
-            context = await self._get_context(message)
+            context = await self._get_context(message) if message else {}
 
-            return await template.render_async(**context)
+            return await template.render_async(**{**context, **extra_context, "message": message})
         except TemplateNotFound:
             self._logger.warning(f"Template {template_name} not found.")
             return f"Шаблон не найден для {template_name}"
