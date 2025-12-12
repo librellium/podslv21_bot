12118b6d32051aba367c7a92ae4f1d1cc5c07a7e
Librellium <246878136+librellium@users.noreply.github.com>
2025-12-12 14:49:02 +0500

    Refactor bot builder: - Pass event_handler to routers - Place StartRouter first to prevent override by TextRouter

--- anonflow/bot/builder.py
+++ anonflow/bot/builder.py
@@ -5,32 +5,35 @@ from aiogram import Router
 from anonflow.config import Config
 from anonflow.moderation import ModerationExecutor
 
-from .routers import MediaRouter, StartRouter, TextRouter
-from .utils import MessageManager, TemplateRenderer
+from .routers import InfoRouter, MediaRouter, StartRouter, TextRouter
+from .utils import EventHandler, MessageManager, TemplateRenderer
 
 
-def build(config: Config,
-          message_manager: MessageManager,
-          template_renderer: TemplateRenderer,
-          executor: Optional[ModerationExecutor] = None):
+def build(
+    config: Config,
+    message_manager: MessageManager,
+    template_renderer: TemplateRenderer,
+    executor: Optional[ModerationExecutor] = None,
+    event_handler: Optional[EventHandler] = None
+):
     main_router = Router()
 
     main_router.include_routers(
-        StartRouter(
-            template_renderer=template_renderer
+        StartRouter(template_renderer=template_renderer),
+        InfoRouter(template_renderer=template_renderer),
+        MediaRouter(
+            config=config,
+            message_manager=message_manager,
+            template_renderer=template_renderer,
+            moderation_executor=executor,
         ),
         TextRouter(
             config=config,
             message_manager=message_manager,
             template_renderer=template_renderer,
-            moderation_executor=executor
+            moderation_executor=executor,
+            event_handler=event_handler
         ),
-        MediaRouter(
-            config=config,
-            message_manager=message_manager,
-            template_renderer=template_renderer,
-            moderation_executor=executor
-        )
     )
 
     return main_router
