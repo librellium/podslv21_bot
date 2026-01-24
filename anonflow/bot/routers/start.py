ec6e399b0d0ebefa2bf6f5aff60880bc6e80bd36
Librellium <246878136+librellium@users.noreply.github.com>
2026-01-25 01:05:29 +0500

    Update start router to save users

--- anonflow/bot/routers/start.py
+++ anonflow/bot/routers/start.py
@@ -2,19 +2,20 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-from anonflow.database import Database
+from anonflow.database import UserRepository
 from anonflow.translator import Translator
 
 
 class StartRouter(Router):
-    def __init__(self, database: Database, translator: Translator):
+    def __init__(self, translator: Translator, user_repository: UserRepository):
         super().__init__()
 
-        self.database = database
         self.translator = translator
+        self.user_repository = user_repository
 
     def setup(self):
         @self.message(CommandStart())
         async def on_start(message: Message):
+            await self.user_repository.add(message.chat.id)
             _ = self.translator.get()
             await message.answer(_("messages.command.start", message=message))
