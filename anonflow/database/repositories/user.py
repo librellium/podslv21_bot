07c04a69f2a79034b8ff8d4aa8b11971be25c9b9
Librellium <246878136+librellium@users.noreply.github.com>
2026-02-04 21:34:46 +0500

    Make UserRepository inherit from BaseRepository

--- anonflow/database/repositories/user.py
+++ anonflow/database/repositories/user.py
@@ -1,80 +1,44 @@
-import asyncio
-import logging
-
 from aiogram.types import ChatIdUnion
-from cachetools import TTLCache
-from sqlalchemy import exists, update
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload, joinedload
 
-from anonflow.database.database import Database
+from .base import BaseRepository
 from anonflow.database.orm import User
 
 
-class UserRepository:
-    def __init__(self, db: Database, cache_size: int, cache_ttl: int):
-        self._logger = logging.getLogger(__name__)
+class UserRepository(BaseRepository):
+    model = User
 
-        self._database = db
-        self._cache = TTLCache(maxsize=cache_size, ttl=cache_ttl)
-        self._cache_lock = asyncio.Lock()
+    async def add(self, session: AsyncSession, chat_id: ChatIdUnion):
+        await super()._add(
+            session,
+            model_args={"chat_id": chat_id}
+        )
 
-    async def add(self, chat_id: ChatIdUnion):
-        async with self._database.get_session() as session:
-            try:
-                user = User(chat_id=chat_id)
-                session.add(user)
-                await session.commit()
+    async def get(self, session: AsyncSession, chat_id: ChatIdUnion):
+        return await super()._get(
+            session,
+            filters={"chat_id": chat_id},
+            options=[
+                selectinload(User.bans),
+                joinedload(User.moderator)
+            ]
+        )
 
-                async with self._cache_lock:
-                    self._cache[chat_id] = user
-            except IntegrityError:
-                await session.rollback()
-                self._logger.warning("User chat_id=%s already exists.", chat_id)
+    async def has(self, session: AsyncSession, chat_id: ChatIdUnion):
+        return await super()._has(
+            session,
+            filters={"chat_id": chat_id}
+        )
 
-    async def block(self, chat_id: ChatIdUnion):
-        await self.update(chat_id, is_blocked=True)
+    async def remove(self, session: AsyncSession, chat_id: ChatIdUnion):
+        await super()._remove(
+            session,
+            filters={"chat_id": chat_id}
+        )
 
-    async def get(self, chat_id: ChatIdUnion):
-        async with self._cache_lock:
-            user = self._cache.get(chat_id)
-            if user: return user
-
-        async with self._database.get_session() as session:
-            result = await session.execute(
-                select(User).where(User.chat_id == chat_id)
-            )
-            return result.scalar_one_or_none()
-
-    async def has(self, chat_id: ChatIdUnion):
-        async with self._cache_lock:
-            if self._cache.get(chat_id):
-                return True
-
-        async with self._database.get_session() as session:
-            result = await session.execute(
-                select(exists().where(User.chat_id == chat_id))
-            )
-            return result.scalar()
-
-    async def unblock(self, chat_id: ChatIdUnion):
-        await self.update(chat_id, is_blocked=False)
-
-    async def update(self, chat_id: ChatIdUnion, **fields):
-        async with self._database.get_session() as session:
-            try:
-                await session.execute(
-                    update(User)
-                    .where(User.chat_id == chat_id)
-                    .values(**fields)
-                    .execution_options(synchronize_session="fetch")
-                )
-                await session.commit()
-
-                user = await session.get(User, chat_id)
-                async with self._cache_lock:
-                    if user:
-                        self._cache[chat_id] = user
-            except IntegrityError:
-                await session.rollback()
-                self._logger.warning("Failed to update user chat_id=%s", chat_id)
+    async def update(self, session: AsyncSession, chat_id: ChatIdUnion, **fields):
+        await super()._update(
+            session,
+            filters={"chat_id": chat_id}, fields=fields
+        )
