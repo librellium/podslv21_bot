ae9784c1e4af423e69098b21d6cfcc1da0522c70
Librellium <246878136+librellium@users.noreply.github.com>
2026-02-14 13:57:13 +0500

    Enhance Translator context and safe string formatting

--- anonflow/translator/translator.py
+++ anonflow/translator/translator.py
@@ -1,4 +1,5 @@
 import gettext
+from collections import defaultdict
 from functools import lru_cache
 from typing import Optional, Literal
 
@@ -6,6 +7,8 @@ from aiogram import Bot
 from aiogram.types import Message
 
 from anonflow import __version_str__, paths
+
+
 
 
 class Translator:
@@ -29,19 +32,29 @@ class Translator:
         msg_context = {}
         if isinstance(message, Message):
             user = message.from_user
+            chat = message.chat
+
+            first_name = getattr(user, "first_name", "")
+            last_name = getattr(user, "last_name", "")
+
             msg_context = {
-                "first_name": getattr(user, "first_name", ""),
-                "last_name": getattr(user, "last_name", ""),
+                "chat_id": getattr(chat, "id", ""),
+                "user_id": getattr(user, "id", ""),
+                "first_name": first_name,
+                "last_name": last_name,
+                "full_name": " ".join(filter(None, (first_name, last_name))),
                 "username": getattr(user, "username", ""),
                 "bot_first_name": getattr(bot, "first_name", ""),
                 "bot_last_name": getattr(bot, "last_name", ""),
                 "bot_username": getattr(bot, "username", ""),
-                "bot_version": __version_str__,
+                "bot_version": __version_str__
             }
 
-        return s.format(
-            **msg_context,
-            **extra
+        return s.format_map(
+            defaultdict(
+                str,
+                msg_context | extra
+            )
         )
 
     def get(self, lang: Literal["ru"] = "ru"):
