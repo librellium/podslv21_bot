55c88ca5f3242a6fd881f2debf91491501369de8
Librellium <246878136+librellium@users.noreply.github.com>
2026-02-15 22:29:30 +0500

    Replace chat_id with user_id in database repositories and related services

--- anonflow/database/repositories/ban.py
+++ anonflow/database/repositories/ban.py
@@ -1,4 +1,3 @@
-from aiogram.types import ChatIdUnion
 from sqlalchemy import func, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -6,36 +5,36 @@ from anonflow.database.orm import Ban
 
 
 class BanRepository:
-    async def ban(self, session: AsyncSession, actor_chat_id: ChatIdUnion, chat_id: ChatIdUnion):
+    async def ban(self, session: AsyncSession, actor_user_id: int, user_id: int):
         async with session.begin():
             ban = Ban(
-                chat_id=chat_id,
-                banned_by=actor_chat_id
+                user_id=user_id,
+                banned_by=actor_user_id
             )
             session.add(ban)
 
-    async def is_banned(self, session: AsyncSession, chat_id: ChatIdUnion):
+    async def is_banned(self, session: AsyncSession, user_id: int):
         result = await session.execute(
             select(Ban)
             .where(
-                Ban.chat_id == chat_id,
+                Ban.user_id == user_id,
                 Ban.is_active.is_(True)
             )
             .limit(1)
         )
         return bool(result.scalar_one_or_none())
 
-    async def unban(self, session: AsyncSession, actor_chat_id: ChatIdUnion, chat_id: ChatIdUnion):
+    async def unban(self, session: AsyncSession, actor_user_id: int, user_id: int):
         async with session.begin():
             await session.execute(
                 update(Ban)
                 .where(
-                    Ban.chat_id == chat_id,
+                    Ban.user_id == user_id,
                     Ban.is_active.is_(True)
                 )
                 .values(
                     is_active=False,
                     unbanned_at=func.now(),
-                    unbanned_by=actor_chat_id
+                    unbanned_by=actor_user_id
                 )
             )
