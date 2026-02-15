94528029d4e29652a78d78fbbb09d05ff36b1cea
Librellium <246878136+librellium@users.noreply.github.com>
2026-02-15 22:13:59 +0500

    Cleanup bot __init__ module

--- anonflow/bot/__init__.py
+++ anonflow/bot/__init__.py
@@ -1,15 +1,1 @@
-from .builder import build
-from .middleware import (
-    BlockedMiddleware,
-    SubscriptionMiddleware,
-    ThrottlingMiddleware,
-    UnregisteredMiddleware
-)
 
-__all__ = [
-    "build",
-    "BlockedMiddleware",
-    "SubscriptionMiddleware",
-    "ThrottlingMiddleware",
-    "UnregisteredMiddleware"
-]
