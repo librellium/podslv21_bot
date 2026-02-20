dd6d881fc181b8d01442bc025333b80d1b58a8b8
Librellium <246878136+librellium@users.noreply.github.com>
2026-02-20 23:31:53 +0500

    Adapt MediaRouter to new Result objects and simplify media processing

--- anonflow/bot/routers/media.py
+++ anonflow/bot/routers/media.py
@@ -1,12 +1,13 @@
 import asyncio
 from asyncio import CancelledError
-from typing import Dict, List, Optional
+from contextlib import suppress
+from typing import Dict, FrozenSet, List
 
 from aiogram import F, Router
 from aiogram.enums import ChatType
 from aiogram.types import Message
 
-from anonflow.config import Config
+from anonflow.config.models import ForwardingType
 from anonflow.moderation import ModerationExecutor
 from anonflow.services.transport import MessageRouter
 from anonflow.services.transport.content import (
@@ -14,69 +15,64 @@ from anonflow.services.transport.content import (
     ContentMediaItem,
     MediaType
 )
-from anonflow.services.transport.events import (
-    ModerationDecisionEvent,
-    PostPreparedEvent
+from anonflow.services.transport.results import (
+    ModerationDecisionResult,
+    PostPreparedResult
 )
 
 
 class MediaRouter(Router):
     def __init__(
         self,
-        config: Config,
         message_router: MessageRouter,
-        moderation_executor: Optional[ModerationExecutor] = None,
+        forwarding_types: FrozenSet[ForwardingType],
+        moderation_executor: ModerationExecutor,
     ):
         super().__init__()
 
-        self.config = config
         self.message_router = message_router
+        self.forwarding_types = forwarding_types
         self.moderation_executor = moderation_executor
 
         self.media_groups: Dict[str, List[Message]] = {}
         self.media_groups_tasks: Dict[str, asyncio.Task] = {}
         self.media_groups_lock = asyncio.Lock()
 
+    def _can_send_media(self, msgs: List[Message]):
+        return any(
+            (msg.photo and "photo" in self.forwarding_types) or
+            (msg.video and "video" in self.forwarding_types)
+            for msg in msgs
+        )
+
+    def _get_media(self, message: Message):
+        if message.photo and "photo" in self.forwarding_types:
+            return {"type": MediaType.PHOTO, "file_id": message.photo[-1].file_id}
+        elif message.video and "video" in self.forwarding_types:
+            return {"type": MediaType.VIDEO, "file_id": message.video.file_id}
+
     def setup(self):
-        def can_send_media(msgs: List[Message]):
-            forwarding_types = self.config.forwarding.types
-            return any(
-                (msg.photo and "photo" in forwarding_types) or
-                (msg.video and "video" in forwarding_types)
-                for msg in msgs
-            )
-
-        def get_media(message: Message):
-            if message.photo and "photo" in self.config.forwarding.types:
-                return {"type": MediaType.PHOTO, "file_id": message.photo[-1].file_id}
-            elif message.video and "video" in self.config.forwarding.types:
-                return {"type": MediaType.VIDEO, "file_id": message.video.file_id}
-
         async def process_messages(messages: List[Message]):
             if not messages:
                 return
 
-            if can_send_media(messages):
-                moderation = self.config.moderation.enabled
-                moderation_approved = not moderation
+            if self._can_send_media(messages):
+                moderation_approved = False
 
                 content_group = ContentMediaGroup()
-                executor = self.moderation_executor
-                for index, message in enumerate(messages):
-                    if moderation and executor:
-                        async for event in executor.process_message(message):
-                            if isinstance(event, ModerationDecisionEvent):
-                                moderation_approved = event.approved
-                            await self.message_router.dispatch(event, message)
+                caption = next((msg.caption for msg in messages if msg.caption), "")
+                for message in messages:
+                    async for result in self.moderation_executor.process_message(message):
+                        if isinstance(result, ModerationDecisionResult):
+                            moderation_approved = result.is_approved
+                        await self.message_router.dispatch(result, message)
 
-                    caption = message.caption if index == 0 else None
-
-                    media = get_media(message)
+                    media = self._get_media(message)
                     if media:
                         content_group.items.append(ContentMediaItem(**media, caption=caption))
 
                 await self.message_router.dispatch(
-                    PostPreparedEvent(content_group, moderation_approved),
+                    PostPreparedResult(content_group, moderation_approved),
                     messages[0]
                 )
 
@@ -88,15 +84,13 @@ class MediaRouter(Router):
             media_group_id = message.media_group_id
 
             async def await_media_group():
-                try:
+                with suppress(CancelledError):
                     await asyncio.sleep(2)
                     async with self.media_groups_lock:
                         messages = self.media_groups.pop(media_group_id, []) # type: ignore
                         self.media_groups_tasks.pop(media_group_id, None) # type: ignore
 
                     await process_messages(messages)
-                except CancelledError:
-                    pass
 
             if media_group_id:
                 async with self.media_groups_lock:
